37192b916b20fbbe2453fd142245c6c3361269e1
xiaohuang1204 <42427875+xiaohuang1204@users.noreply.github.com>
2018-08-15 22:02:55 -0700

    Add files via upload

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gorilla/handlers"
+	"net/http"
+	"os"
+)
+
+func HelloWorld(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hello World Veritas!!"))
+}
+
+func main() {
+	fmt.Printf("Starting Hello World application... \n")
+	http.HandleFunc("/", HelloWorld)
+	http.ListenAndServe(":8080", handlers.LoggingHandler(os.Stdout, http.DefaultServeMux))
+}
